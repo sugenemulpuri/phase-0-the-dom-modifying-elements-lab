a2b5974e416900b5ce2a2e08d08f754ff3f5ce86
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-07 11:07:10 -0600

    phase 0 the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+
+const main = document.querySelector("main#main")
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+
+const victory = document.getElementById("victory");
+
+
+const name = "Sugene is the champion";
+
+newHeader.innerHTML = name;
+
+
+
+// document.getElementById("victory").textContent = "Sugene is the champion";
+
